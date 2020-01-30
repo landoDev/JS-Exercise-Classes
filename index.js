1be569bf61f67e33d5e713370a66e43219c01ad4
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-01-30 14:01:40 -0500

    passed student class test

--- index.js
+++ index.js
@@ -160,7 +160,22 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(personalInfo){
+    super(personalInfo)
+    this.previousBackground = personalInfo.previousBackground;
+    this.className = personalInfo. className;
+    this.favSubjects = personalInfo.favSubjects;
+  }
+  listSubjects(){
+    return `${this.favSubjects}`;
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject};`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 
 }
 
